4ac01bff8809d87c2026027c662d5a6925bf5dd1
hantsy <hantsy@gmail.com>
2021-08-29 18:38:36 +0800

    chore: clean codes

--- csharp/TransactionAnalyzer.Lib.Tests/FakeTransactionLoader.cs
+++ csharp/TransactionAnalyzer.Lib.Tests/FakeTransactionLoader.cs
@@ -8,8 +8,8 @@
         LFVCTEYM, 20/08/2020 12:50:02, 5.00, MacLaren, PAYMENT,
         SUOVOISP, 20/08/2020 13:12:22, 5.00, Kwik-E-Mart, PAYMENT,
         AKNBVHMN, 20/08/2020 13:14:11, 10.95, Kwik-E-Mart, REVERSAL, YGXKOEIA
-            JYAPKZFZ, 20/08/2020 14:07:10, 99.50, MacLaren, PAYMENT,
-            */
+        JYAPKZFZ, 20/08/2020 14:07:10, 99.50, MacLaren, PAYMENT,
+        */
         public Transaction[] Load()
         {
             return Fixtures.Load();
