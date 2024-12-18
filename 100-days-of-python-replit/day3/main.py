ec0110906be8df74c01733b6828ddc91ab8bb650
hantsy <hantsy@gmail.com>
2024-12-18 15:32:53 +0800

    chore: clean codes

---
+++ 100-days-of-python-replit/day3/main.py
@@ -0,0 +1,10 @@
+food = input("Name a type of food: ")
+plant = input("Name a plant: ")
+cookingType = input("What is a way to cook something? ")
+burntFood = input("How do you describe burnt food? ")
+householdItem = input("Name something in your house: ")
+
+print()
+print("Tonight's dinner:")
+
+print("For dinner you should make", cookingType, food, "with", burntFood, plant, "on a plate of", householdItem)
