701067e756240e72dd42da1efd881676e2919fd5
hantsy <hantsy@gmail.com>
2024-12-18 15:32:12 +0800

    chore: rename

--- 100days-of-python-replit/day1/main.py
+++
@@ -1,8 +0,0 @@
-print("Hantsy Bai")
-print("December 15th 2024")
-print("""I am signing up for Replit's 100 days of Python challenge!
-I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
-I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!"""
-      )
-print("I am feeling 😊")
-print("You can follow my progress at replit.com/@hantsy")
