452086a8b214201f1be5cf0852856e8f7fdf90ee
hantsy <hantsy@gmail.com>
2024-12-19 12:53:44 +0800

    chore: day 4

---
+++ 100-days-of-python-replit/day4/main.py
@@ -0,0 +1,36 @@
+print("Welcome to your Adventure Story Simulator.")
+print()
+print(
+    "I am going to ask you a bunch of questions and then create an epic story with you as the star."
+)
+
+print()
+name = input("What is your name? ")
+print()
+enemyName = input("What is your enemy's name? ")
+print()
+superPower = input("What is your super power? ")
+print()
+live = input("Where do you live? ")
+print()
+food = input("What is your favorite food? ")
+
+print()
+print(
+    "Hello",
+    name,
+    "Your ability to",
+    superPower,
+    "will make sure you never have to look at",
+    "\033[31m",
+    enemyName,
+    "\033[0m",
+    "again.",
+    "Go eat",
+    food,
+    "as you walk down the streets of",
+    live,
+    "and use",
+    superPower,
+    "for good and not evil!",
+)
